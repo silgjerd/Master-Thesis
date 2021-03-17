2ee7f2b1d3b02cf6a459b17c82ec20e0b078b740
Ola <ola@silgjerd.com>
2021-03-17 10:20:57 +0100

    exploration alpha

--- exploration.R
+++ exploration.R
@@ -3,61 +3,74 @@ library(tidyverse);library(vroom)
 library(PerformanceAnalytics);library(RColorBrewer);library(corrplot)
 source("functions.R")
 
-df <- vroom("data/dataset.csv")
-df <- df %>% na.omit
+df <- vroom("data/traintest_allwithidentifiers.csv")
 
-# Exclude variables
-df <- df %>%
-  select(-c(
-    PERMNO,
-    SICCD,
-    PRC,
-    VOL,
-    SHROUT,
-    LOGRET,
-    ymon,
-    FF_12
-  ))
+
+
+
+# =========================================================================
+# ALPHA (move to seperate file?) ATTEMPT 2
+# =========================================================================
+
+output <- c()
+
+quant <- 1/10
+ymons <- unique(df$ymon)
+
+for (cvar in colnames(df)[1:67]){
+  
+  cat(cvar, "\n")
+  
+  for (cymon in ymons){
+    
+    
+    cdf <- df %>% filter(ymon == cymon)
+    
+    # sort
+    corder <- order(cdf[cvar])
+    cdf <- cdf[corder,]
+    
+    # rets
+    ret_long  <- cdf$LOGRET %>% tail(floor(nrow(cdf)*quant))
+    ret_short <- cdf$LOGRET %>% head(floor(nrow(cdf)*quant))
+    ret_net   <- ret_long - ret_short
+    
+    # output
+    output <- output %>% bind_rows(tibble("meanlong" = mean(ret_long),
+                                          "sdlong" = sd(ret_long),
+                                          "meanshort" = mean(ret_short),
+                                          "sdshort" = sd(ret_short),
+                                          "meannet" = mean(ret_net),
+                                          "sdnet" = sd(ret_net),
+                                          "n" = length(ret_net),
+                                          "var" = cvar
+    ))
+    
+  }
+}
+
+aggtest <- output %>%
+  group_by(var) %>%
+  summarise(ret = mean(meannet),
+            sd  = sd(meannet),
+            n = n())
+
+aggtest <- aggtest %>%
+  mutate(tstat = ret / (sd / sqrt(n))) %>%
+  arrange(desc(abs(tstat)))
+
+
+
+
+
+
+
 
 
 # =========================================================================
 # Correlation matrix scatterplot
 # =========================================================================
-# Past returns
-plotdat <- df %>%
-  #select(RET,
-  #       chmom, r12_2, r12_7, r2_1, r36_13, r6_2, strev) %>%
-  sample_n(1000)
 
-
-chart.Correlation(plotdat, histogram = T, pch = 19)
-
-col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
 M <- cor(df)
-cor.mtest <- function(mat, ...) {
-  mat <- as.matrix(mat)
-  n <- ncol(mat)
-  p.mat<- matrix(NA, n, n)
-  diag(p.mat) <- 0
-  for (i in 1:(n - 1)) {
-    for (j in (i + 1):n) {
-      tmp <- cor.test(mat[, i], mat[, j], ...)
-      p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
-    }
-  }
-  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
-  p.mat
-}
-# matrix of the p-value of the correlation
-p.mat <- cor.mtest(plotdat)
-corrplot(M, method="color", col=col(200),
-         type="upper", order="hclust", 
-         addCoef.col = "black", # Add coefficient of correlation
-         tl.col="black", tl.srt=45, #Text label color and rotation
-         # Combine with significance
-         p.mat = p.mat, sig.level = 0.01, insig = "blank", 
-         # hide correlation coefficient on the principal diagonal
-         diag=FALSE 
-)
+corrplot(M, method = "color", type = "lower")
 
-corrplot(M, method = "color")
