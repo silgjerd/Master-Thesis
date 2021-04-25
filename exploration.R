0d23c4a9a055674d65148d4599def22d4a54f299
Ola <ola@silgjerd.com>
2021-04-25 11:39:36 +0200

    moved to seperate file

--- exploration.R
+++ exploration.R
@@ -3,65 +3,7 @@ library(tidyverse);library(vroom)
 library(PerformanceAnalytics);library(RColorBrewer);library(corrplot)
 source("functions.R")
 
-df <- vroom("data/traintest_allwithidentifiers.csv")
-
-
-
-
-# =========================================================================
-# ALPHA (move to seperate file?) ATTEMPT 2
-# =========================================================================
-
-output <- c()
-
-quant <- 1/10
-ymons <- unique(df$ymon)
-
-for (cvar in colnames(df)[1:67]){
-  
-  cat(cvar, "\n")
-  
-  for (cymon in ymons){
-    
-    
-    cdf <- df %>% filter(ymon == cymon)
-    
-    # sort
-    corder <- order(cdf[cvar])
-    cdf <- cdf[corder,]
-    
-    # rets
-    ret_long  <- cdf$LOGRET %>% tail(floor(nrow(cdf)*quant))
-    ret_short <- cdf$LOGRET %>% head(floor(nrow(cdf)*quant))
-    ret_net   <- ret_long - ret_short
-    
-    # output
-    output <- output %>% bind_rows(tibble("meanlong" = mean(ret_long),
-                                          "sdlong" = sd(ret_long),
-                                          "meanshort" = mean(ret_short),
-                                          "sdshort" = sd(ret_short),
-                                          "meannet" = mean(ret_net),
-                                          "sdnet" = sd(ret_net),
-                                          "n" = length(ret_net),
-                                          "var" = cvar
-    ))
-    
-  }
-}
-
-aggtest <- output %>%
-  group_by(var) %>%
-  summarise(ret = mean(meannet),
-            sd  = sd(meannet),
-            n = n())
-
-aggtest <- aggtest %>%
-  mutate(tstat = ret / (sd / sqrt(n))) %>%
-  arrange(desc(abs(tstat)))
-
-
-
-
+df <- vroom("data/x_exploration.csv")
 
 
 
@@ -73,4 +15,17 @@ aggtest <- aggtest %>%
 
 M <- cor(df)
 corrplot(M, method = "color", type = "lower")
+
+
+
+# =========================================================================
+# Averages over time
+# =========================================================================
+
+
+df %>%
+  group_by(ymon) %>%
+  summarise(n = n()) %>%
+  plot
+  
 
