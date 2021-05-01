996fb863fa95ec648982bc492b14beb896db0c37
Ola <ola@silgjerd.com>
2021-05-01 17:29:26 +0200

    boxplots

--- results_elasticnet.R
+++ results_elasticnet.R
@@ -11,17 +11,17 @@ load("data/x_fullsample.RData")
 
 # Build
 control <- trainControl(method = "repeatedcv",
-                       number = 5,
-                       repeats = 2,
-                       search = "random",
-                       verboseIter = TRUE)
+                        number = 5,
+                        repeats = 5,
+                        search = "random",
+                        verboseIter = TRUE)
 # Train
 elastic_model <- train(RET ~ .,
-                      data = cbind(x_train, y_train),
-                      method = "glmnet",
-                      #preProcess = c("center", "scale"),
-                      tuneLength = 25,
-                      trControl = control)
+                       data = cbind(x_train, y_train),
+                       method = "glmnet",
+                       #preProcess = c("center", "scale"),
+                       tuneLength = 25,
+                       trControl = control)
 
 # Test
 pred <- predict(elastic_model, x_test)
@@ -33,6 +33,7 @@ abline(a=0,b=1)
 abline(a=summary(lm(y_test~pred))$coef[1],b=summary(lm(y_test~pred))$coef[2],col="red")
 
 
+# Variable importance
 
 
 #####################################################################
@@ -79,7 +80,7 @@ esg_e <- c("carbonint",
            "esgres",
            "esgemi",
            "esginn"
-           )
+)
 esg_s <- c("femexec",
            "fememp",
            "turnemp",
@@ -90,7 +91,7 @@ esg_s <- c("femexec",
            "esghum",
            "esgcomm",
            "esgpro"
-           )
+)
 
 esg_g <- c("boardindep",
            "boardfem",
@@ -113,7 +114,7 @@ getEVAL <- function(x_train, y_train, x_test, y_test){
   # Build
   control <- trainControl(method = "repeatedcv",
                           number = 5,
-                          repeats = 1,
+                          repeats = 5,
                           search = "random",
                           verboseIter = TRUE)
   # Train
@@ -136,7 +137,9 @@ getEVAL <- function(x_train, y_train, x_test, y_test){
 output <- c()
 
 samples <- list(
-  "sample" = c("none","e","s","g","es","eg","sg","all"),
+  "sample" = c("none","E","S","G",
+               #"es","eg","sg",
+               "Scores","ESG"),
   "excols" = list(c(which(colnames(x_train) %in% esgvars)),
                   c(which(colnames(x_train) %in% c(esg_s, esg_g))),
                   c(which(colnames(x_train) %in% c(esg_e, esg_g))),
@@ -144,6 +147,7 @@ samples <- list(
                   c(which(colnames(x_train) %in% c(esg_g))),
                   c(which(colnames(x_train) %in% c(esg_s))),
                   c(which(colnames(x_train) %in% c(esg_e))),
+                  c(which(colnames(x_train) %in% c(esg_scores))),
                   NULL)
 )
 
@@ -161,7 +165,7 @@ for (sample in samples$sample){
   }
   
   
-  for (i in 1:20){
+  for (i in 1:1){
     cat(i,"\n")
     
     eval <- getEVAL(cur_x_train, y_train, cur_x_test, y_test)
@@ -181,6 +185,10 @@ for (sample in samples$sample){
 
 
 
+# ===========================================================================
+# PLOTS
+# ===========================================================================
+
 # Boxplot
 
 output %>%
@@ -188,13 +196,57 @@ output %>%
   geom_boxplot(aes(y = MSE))+
   theme_bw()
 
+# Diff
+diff <- output %>%
+  group_by(sample) %>%
+  summarise(meanMSE = mean(MSE),
+            meanRSQ = mean(RSQ))
+
+diff <- diff %>%
+  mutate(diffMSE = meanMSE - diff$meanMSE[diff$sample=="none"],
+         diffRSQ = meanRSQ - diff$meanRSQ[diff$sample=="none"])
+
+
+#barplot
+diff %>%
+  ggplot()+
+  geom_point(aes(x=sample,y=diffMSE))+
+  geom_hline(yintercept = 0)
+
+output <- output %>%
+  mutate(MSEmcontrol = MSE - diff$meanMSE[diff$sample=="none"],
+         RSQmcontrol = RSQ - diff$meanRSQ[diff$sample=="none"])
+
+#boxplot
+library(plyr)
+output$sample <- revalue(output$sample, c("s"="S","e"="E","g"="G","all"="ESG"))
+output <- output %>% mutate(sample = ordered(sample, c("ESG","G","S","E")))
+
+# forest plot
+output %>%
+  filter(sample!="none") %>%
+  filter(sample!="scores") %>%
+  mutate(sample = as.factor(sample)) %>%
+  ggplot(aes(x = sample))+
+  geom_boxplot(aes(y = MSEmcontrol), outlier.color = "white", width = .3)+
+  geom_hline(yintercept = 0, linetype = "dashed")+
+  labs(x="",y="Difference in MSE")+
+  coord_flip()+
+  #scale_y_reverse()+
+  theme_bw()+theme(panel.grid.major=element_blank(),panel.grid.minor=element_blank())
 
 
 
 
 
+############################################################################
 
 
+# Variable importance
+
+importance <- varImp(elastic_model)
+importance <- importance[["importance"]]
+importance <- rownames_to_column(importance) %>% arrange(-Overall)
 
 
 
